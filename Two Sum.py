71dd7a5e30aa3431f5745ef70425eee84689d896
Brian Schendt <brianschendt@gmail.com>
2018-01-09 21:32:54 -0500

    Two Sum bad

--- Two Sum.py
+++ Two Sum.py
@@ -1,21 +1,47 @@
+import math
+
+
 class Solution(object):
+    def BinarySearch(self, A, value, low, high):
+        if (high < low):
+            return None
+        mid = (low + high)/2
+        if (A[mid] > value):
+            return self.BinarySearch(A, value, low, mid-1)
+        elif (A[mid] < value):
+            return self.BinarySearch(A, value, mid+1, high)
+        else:
+            return mid
+
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        d = dict()
-        for i in range(len(nums)):
-            for j in range(1, len(nums)):
-                if (i != j):
-                    val = nums[i]+nums[j]
-                    d[str(val)] = [i, j]
-        if (str(target) in d):
-            return d[str(target)]
-        else:
-            return None
+        snums = sorted(nums)
+        print snums
+        n = int(math.ceil(len(snums)/2.0))
+        print "n:" + str(n)
+        for i in range(0, n+1):
+            print "i:" + str(i)
+            print snums[i]
+            print(target-snums[i])
+            print ("-------")
+            j = self.BinarySearch(snums, target - snums[i], 0, len(snums)-1)
+            if (j is not None):
+                print("hit!")
+                print(snums[i])
+                print(snums[j])
+                print(nums)
+                return []
+
+
 
 
 s = Solution()
-print(s.twoSum([5, 6, 7], 11))
+#nums = [8, 11, 10, 12, 7, 5, 24]
+#snums = sorted(nums)
+#print snums
+#print(s.BinarySearch(snums, 8, 0, len(nums)-1))
+print(s.twoSum([8, 11, 10, 12, 7, 5, 24], 13))
