ebf2104ac371d32e2c58b96b295b59665e5001f5
Brian Schendt <brianschendt@gmail.com>
2018-01-15 15:30:44 -0500

    Solution

--- Invert Binary Tree.py
+++ Invert Binary Tree.py
@@ -11,39 +11,35 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
-        L = list()
-        self.preOrder(root, L)
-        # print(L[1:])
-        newroot = TreeNode(L[0])
-        for val in L[1:]:
-            self.put(TreeNode(val), newroot)
+        if not root:
+            return []
 
-        W = list()
-        self.preOrder(newroot, W)
-        print(W)
+        newroot = TreeNode(root.val)
+        self.preOrder(root, newroot, 0)
+        return newroot
 
-    def preOrder(self, root, L):
+    def preOrder(self, root, newroot, direction):
         if root:
-            L.append(root.val)
-            self.preOrder(root.left, L)
-            self.preOrder(root.right, L)
+            if (direction == -1):
+                #print(direction, newroot.val)
+                newroot.right = TreeNode(root.val)
+                newroot = newroot.right
+            elif (direction == 1):
+                #print(direction, newroot.val)
+                newroot.left = TreeNode(root.val)
+                newroot = newroot.left
+            print(root.val)
+            self.preOrder(root.left, newroot, -1)
+            self.preOrder(root.right, newroot, 1)
 
-    def put(self, node, rel_root):
-        if node.val < rel_root.val:
-            if rel_root.left:
-                self.put(node, rel_root.left)
-            else:
-                rel_root.left = node
-        else:
-            if rel_root.right:
-                self.put(node, rel_root.right)
-            else:
-                rel_root.right = node
 
 s = Solution()
 L = list()
 for i in range(10):
     L.append(TreeNode(i))
+
+#root = L[1]
+#root.left = L[2]
 
 L[4].left = L[2]
 L[4].right = L[7]
@@ -52,4 +48,8 @@ L[2].right = L[3]
 L[7].left = L[6]
 L[7].right = L[9]
 
-s.invertTree(L[4])
+newroot = s.invertTree(L[4])
+print("------------")
+print(newroot.val)
+#print(newroot.left.val)
+#print(newroot.right.val)
