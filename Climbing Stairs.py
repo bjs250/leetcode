a444f438a09fe545c1c8532de740d39983dc53ce
Brian Schendt <brianschendt@gmail.com>
2018-01-19 18:04:26 -0500

    Solved faster

--- Climbing Stairs.py
+++ Climbing Stairs.py
@@ -4,27 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        self.m = 0
-        self.d = dict()
-        self.d[1] = 1
-        self.d[2] = 2
-        return self.Fib(n)
+        L = [1, 1]
+        for i in range(2, n+1):
+            L.append(L[-1] + L[-2])
+        return L[n]
 
-    def Fib(self, n):
-        if n == 1:
-            return 1
-        elif n == 2:
-            return 2
-        else:
-            try:
-                n2 = self.d[n-2]
-            except:
-                n2 = self.Fib(n-2)
-            try:
-                n1 = self.d[n-1]
-            except:
-                n1 = self.Fib(n-1)
-            return self.m + n1 + n2
 
 s = Solution()
 print s.climbStairs(35)
