259d20f8311bdac8ea66dfbec698638feec8bba5
Brian Schendt <brianschendt@gmail.com>
2018-01-21 14:15:53 -0500

    Solved

---
+++ Power of Three.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 1:
+            return False
+        while n != 1:
+            if n % 3 == 0:
+                n = n /3
+            else:
+                return False
+        return True
+
+        
