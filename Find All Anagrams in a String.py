469fece21bfc557a00810a85ff8fd4bfd1225aed
Brian Schendt <brianschendt@gmail.com>
2018-01-26 15:00:46 -0500

    Solved

--- Find All Anagrams in a String.py
+++ Find All Anagrams in a String.py
@@ -1,24 +1,22 @@
+from collections import Counter
+
 class Solution(object):
+
     def findAnagrams(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: List[int]
         """
-        L = list()
-        bag = set(p)
-        for i in range(0, len(s) - len(p) + 1):
-            end = i + len(p)
-            print(bag)
-            if bag.intersection(s[i:end]) == bag:
-                L.append(i)
-        return L
-
-sol = Solution()
-#s = 'cbaebabacd'
-#p = 'abc'
-#s = 'abab'
-#p = 'ab'
-s = 'baa'
-p = 'aa'
-print(sol.findAnagrams(s, p))
+        res = []
+        pCounter = Counter(p)
+        sCounter = Counter(s[:len(p)-1])
+        print(pCounter,sCounter)
+        for i in range(len(p)-1,len(s)):
+            sCounter[s[i]] += 1   # include a new char in the window
+            if sCounter == pCounter:    # This step is O(1), since there are at most 26 English letters
+                res.append(i-len(p)+1)   # append the starting index
+            sCounter[s[i-len(p)+1]] -= 1   # decrease the count of oldest char in the window
+            if sCounter[s[i-len(p)+1]] == 0:
+                del sCounter[s[i-len(p)+1]]   # remove the count if it is 0
+        return res
